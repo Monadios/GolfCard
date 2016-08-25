d7ba2e485fcaea7f440b08c6fec33f975b6e8ca6
Monadios <gfalkenthros@gmail.com>
2016-08-25 15:53:58 +0200

    Added 'Next' and 'Save' Buttons

--- app/src/main/java/com/ckey/golfcard/Putting.java
+++ app/src/main/java/com/ckey/golfcard/Putting.java
@@ -1,56 +1,70 @@
 package com.ckey.golfcard;
 
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.graphics.Color;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.*;
-import android.view.inputmethod.EditorInfo;
 import android.widget.*;
 import org.w3c.dom.Text;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Putting extends AppCompatActivity
 {
     private TableLayout table;
-    private EditText p1score,p2score;
+    private EditText p1score, p2score;
     private ArrayList<Integer> p1scores, p2scores;
+    private Button nextButton;
+    private int curRound;
+    private Map<String, ArrayList<Integer>> scores;
+    private TextView curRoundText;
 
     @Override
     protected void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_putting );
+        curRound = 0;
+        p1scores = new ArrayList<Integer>();
+        p2scores = new ArrayList<Integer>();
+        scores = new HashMap<String,ArrayList<Integer>>();
         p1score = (EditText) findViewById( R.id.p1_field );
         p2score = (EditText) findViewById( R.id.p2_field );
-        p1score.setOnEditorActionListener( new TextView.OnEditorActionListener()
-        {
-            @Override
-            public boolean onEditorAction( TextView textView, int i, KeyEvent keyEvent )
-            {
-                return false;
-            }
-        } );
+        p1score.setText( "" );
+        p2score.setText( "" );
+        curRoundText = (TextView) findViewById( R.id.holetext );
+        curRoundText.setText( "Round " + curRound );
+        nextButton = (Button) findViewById( R.id.next_btn );
 
-        p2score.setOnEditorActionListener( new TextView.OnEditorActionListener()
+        nextButton.setOnClickListener( new View.OnClickListener()
         {
             @Override
-            public boolean onEditorAction( TextView textView, int i, KeyEvent keyEvent )
+            public void onClick( View view )
             {
-                return false;
+                /* TODO
+                * Add error handling instead of just ignoring it
+                *
+                */
+
+                if ( p1score.getText().length() != 0 && p1score.getText().length() != 0 ) {
+
+                    int s1 = Integer.parseInt( p1score.getText().toString());
+                    int s2 = Integer.parseInt( p2score.getText().toString());
+
+                    p1scores.add(s1);
+                    p2scores.add(s2);
+
+                    scores.put( "P1", p1scores);
+                    scores.put( "P2", p2scores);
+                    p1score.setText( "" );
+                    p2score.setText( "" );
+                    curRound++;
+                    curRoundText.setText(String.valueOf( curRound ));
+                }
             }
         } );
     }
-
 
 
 
