f5d7ccb9c5df6199071a55eb6fda5bd52d18d65d
Monadios <gfalkenthros@gmail.com>
2016-08-25 10:37:27 +0200

    Fixed the Layout

--- app/src/main/java/com/ckey/golfcard/Putting.java
+++ app/src/main/java/com/ckey/golfcard/Putting.java
@@ -1,41 +1,57 @@
 package com.ckey.golfcard;
 
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
+import android.view.*;
+import android.view.inputmethod.EditorInfo;
 import android.widget.*;
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Putting extends AppCompatActivity
 {
+    private TableLayout table;
+    private EditText p1score,p2score;
+    private ArrayList<Integer> p1scores, p2scores;
 
     @Override
     protected void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_putting );
-        drawFields( 2, 9 );
+        p1score = (EditText) findViewById( R.id.p1_field );
+        p2score = (EditText) findViewById( R.id.p2_field );
+        p1score.setOnEditorActionListener( new TextView.OnEditorActionListener()
+        {
+            @Override
+            public boolean onEditorAction( TextView textView, int i, KeyEvent keyEvent )
+            {
+                return false;
+            }
+        } );
+
+        p2score.setOnEditorActionListener( new TextView.OnEditorActionListener()
+        {
+            @Override
+            public boolean onEditorAction( TextView textView, int i, KeyEvent keyEvent )
+            {
+                return false;
+            }
+        } );
     }
 
-    private void drawFields(int numPlayers, int numRounds)
-    {
-        TableLayout table = (TableLayout) findViewById( R.id.table_layout );
-        for ( int i = 0; i < numRounds; i++ ) {
-            TableRow round = new TableRow( this );
-            for ( int j = 0; j < numPlayers; j++ ) {
-                EditText text = new EditText( this );
-                text.setLayoutParams( new ActionBar.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT ) );
-                text.setId( i * j );
-                text.setInputType( InputType.TYPE_CLASS_NUMBER );
-                round.addView( text, new TableRow.LayoutParams( i, TableRow.LayoutParams.WRAP_CONTENT, 1f ) );
-            }
-            table.addView( round );
-        }
-    }
+
+
+
 }
